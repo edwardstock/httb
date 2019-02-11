7d471e6b42ad3912e0be1079da94616fc265561f
Eduard Maximovich <edward.vstock@gmail.com>
2019-02-11 15:35:34 +0300

    bincrafters remote

--- conan_install.sh
+++ conan_install.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 
 conan install . -s build_type=Debug --install-folder=conan --build missing
